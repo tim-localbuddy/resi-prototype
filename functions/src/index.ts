d3e7928732e055570a3a873d3b42652594af4c73
Tim Leung <tim.leung@localbuddy.co.uk>
2026-05-10 19:10:29 +0100

    refactor: remove Google Drive integration and associated document fetching logic

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,5 @@ import { setGlobalOptions } from "firebase-functions/v2";
 // Set concurrency and region globally
 setGlobalOptions({ maxInstances: 10, region: "europe-west1" });
 
-export { getDocuments } from "./getDocuments";
 export { askAi } from "./askAi";
 export { getUploadUrl, markDocumentUploaded, getDownloadUrl } from "./documents";
